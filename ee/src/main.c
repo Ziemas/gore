14dd002c372fe72b3493d0ca4a83aaaae5105c5a
Ziemas <ziemas@ziemas.se>
2022-07-05 03:55:11 +0200

    Update IOP modules

--- ee/src/main.c
+++ ee/src/main.c
@@ -1,4 +1,7 @@
+#include <iopcontrol.h>
 #include <sbv_patches.h>
+#include <sifrpc.h>
+#include <stdbool.h>
 
 int
 main()
@@ -6,4 +9,16 @@ main()
     sbv_patch_enable_lmb();
     sbv_patch_disable_prefix_check();
     sbv_patch_fileio();
+
+    SifInitRpc(0);
+
+    while (!SifIopReboot("host:ioprp300.img"))
+        ;
+    while (!SifIopSync())
+        ;
+
+    SifInitRpc(0);
+
+    while (true)
+        ;
 }
